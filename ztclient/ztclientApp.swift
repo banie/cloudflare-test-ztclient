7764fa98e054d3e1a41a234dc09eef209e9782cd
banie <banie.setijoso@gmail.com>
2023-09-15 06:01:48 -0400

    Initial dump for the project: - The take home files and instructions - The dummy hello world to run on macos - The updated gitignore file

---
+++ ztclient/ztclientApp.swift
@@ -0,0 +1,17 @@
+//
+//  ztclientApp.swift
+//  ztclient
+//
+//  Created by banie setijoso on 2023-09-13.
+//
+
+import SwiftUI
+
+@main
+struct ztclientApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
